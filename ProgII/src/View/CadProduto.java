1e627747537a3db44a09ea1c220fc3b16667acba
Caio Lima <caioifg@gmail.com>
2019-07-05 00:13:13 -0300

    Finalizado Inserção de Imagem

--- ProgII/src/View/CadProduto.java
+++ ProgII/src/View/CadProduto.java
@@ -3,13 +3,20 @@ package View;
 import java.awt.Color;
 import java.awt.EventQueue;
 import java.awt.Font;
+import java.awt.Panel;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.File;
+import java.util.ArrayList;
 
+import javax.swing.DefaultComboBoxModel;
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JComboBox;
+import javax.swing.JFileChooser;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
@@ -18,7 +25,16 @@ import javax.swing.UIManager;
 import javax.swing.border.BevelBorder;
 import javax.swing.border.EmptyBorder;
 import javax.swing.border.TitledBorder;
+import javax.swing.filechooser.FileNameExtensionFilter;
 import javax.swing.table.DefaultTableModel;
+
+
+import Controller.CategoriaController;
+import Controller.MarcaController;
+import Controller.ProdutoController;
+import Model.Categoria;
+import Model.Marca;
+import Model.Produto;
 
 public class CadProduto extends JFrame {
 
@@ -27,11 +43,13 @@ public class CadProduto extends JFrame {
 	private JTextField txtValVenda;
 	private JTextField txtDescProd;
 	private JTextField txtSaldoEstoque;
-	private JTable tableProduto;
 	private JTextField txtImagem;
 	private String modo;
 	private JButton btnNovo;
 	private JButton btnSalvar;
+	private JComboBox cbCategoria;
+	private JTextField txtUnidMedida;
+	private JLabel lbImagem;
 
 	/**
 	 * Launch the application.
@@ -52,10 +70,13 @@ public class CadProduto extends JFrame {
 	/**
 	 * Create the frame.
 	 */
+	
+
+
 	public CadProduto() {
 		setTitle("Cadastro Produto");
 		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-		setBounds(100, 100, 825, 570);
+		setBounds(100, 100, 825, 395);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
@@ -68,7 +89,7 @@ public class CadProduto extends JFrame {
 		
 		JPanel panel = new JPanel();
 		panel.setBorder(new TitledBorder(UIManager.getBorder("TitledBorder.border"), "Produto", TitledBorder.LEADING, TitledBorder.TOP, null, new Color(0, 0, 0)));
-		panel.setBounds(10, 36, 789, 228);
+		panel.setBounds(10, 36, 789, 314);
 		contentPane.add(panel);
 		panel.setLayout(null);
 		
@@ -77,163 +98,180 @@ public class CadProduto extends JFrame {
 		panel.add(lblNomeProduto);
 		
 		txtNomeProd = new JTextField();
-		txtNomeProd.setBounds(10, 39, 305, 20);
+		txtNomeProd.setBounds(10, 39, 385, 20);
 		panel.add(txtNomeProd);
 		txtNomeProd.setColumns(10);
 		
-		JLabel lblValorVenda = new JLabel("Valor Venda");
-		lblValorVenda.setBounds(368, 25, 86, 14);
+		JLabel lblValorVenda = new JLabel("Pre\u00E7o Venda");
+		lblValorVenda.setBounds(405, 25, 86, 14);
 		panel.add(lblValorVenda);
 		
 		txtValVenda = new JTextField();
-		txtValVenda.setBounds(368, 39, 86, 20);
+		txtValVenda.setBounds(405, 39, 65, 20);
 		panel.add(txtValVenda);
 		txtValVenda.setColumns(10);
 		
 		JLabel lblCategoria = new JLabel("Categoria");
 		lblCategoria.setBounds(587, 25, 74, 14);
 		panel.add(lblCategoria);
-		
+		//===============================================================================
+		//PREENCHENDO O COMBO BOX
 		JComboBox cbCategoria = new JComboBox();
+		cbCategoria.addItem("");
+		CategoriaController catControl = new CategoriaController();
+		
+		for(Categoria categoria: catControl.buscarCategoriasController()) {
+			cbCategoria.addItem(categoria);
+		}
 		cbCategoria.setBounds(587, 39, 192, 20);
 		panel.add(cbCategoria);
 		
 		JLabel lblDescrio = new JLabel("Descri\u00E7\u00E3o");
-		lblDescrio.setBounds(10, 70, 74, 14);
+		lblDescrio.setBounds(10, 87, 74, 14);
 		panel.add(lblDescrio);
 		
 		txtDescProd = new JTextField();
-		txtDescProd.setBounds(10, 84, 444, 20);
+		txtDescProd.setBounds(10, 106, 385, 20);
 		panel.add(txtDescProd);
 		txtDescProd.setColumns(10);
 		
 		JLabel lblMarca = new JLabel("Marca");
-		lblMarca.setBounds(587, 70, 46, 14);
+		lblMarca.setBounds(587, 87, 46, 14);
 		panel.add(lblMarca);
 		
 		JComboBox cbMarca = new JComboBox();
-		cbMarca.setBounds(587, 84, 192, 20);
+		cbMarca.addItem("");
+		MarcaController marcaControl = new MarcaController();
+		
+		for(Marca marca: marcaControl.buscarMarcaController()) {
+			cbMarca.addItem(marca);
+		}
+		cbMarca.setBounds(587, 106, 192, 20);
 		panel.add(cbMarca);
 		
-		JLabel lblEstoque = new JLabel("Saldo Estoque");
-		lblEstoque.setBounds(479, 25, 86, 14);
+		JLabel lblEstoque = new JLabel("Saldo");
+		lblEstoque.setBounds(405, 87, 86, 14);
 		panel.add(lblEstoque);
 		
 		txtSaldoEstoque = new JTextField();
-		txtSaldoEstoque.setBounds(479, 39, 86, 20);
+		txtSaldoEstoque.setBounds(405, 106, 51, 20);
 		panel.add(txtSaldoEstoque);
 		txtSaldoEstoque.setColumns(10);
-		
-		JButton btnSalvar = new JButton("Salvar");
-		btnSalvar.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				modo = "Salvar";
-			}
-		});
-		btnSalvar.setBounds(671, 139, 89, 23);
-		panel.add(btnSalvar);
-		btnSalvar.setEnabled(false);
-		
-		JButton btnEditar = new JButton("Editar");
-		btnEditar.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-			}
-		});
-		btnEditar.setBounds(572, 173, 89, 23);
-		panel.add(btnEditar);
-		
-		JButton btnExcluir = new JButton("Excluir");
-		btnExcluir.setBounds(671, 173, 89, 23);
-		panel.add(btnExcluir);
+		;
 		
 		JLabel lblImagem = new JLabel("Imagem");
-		lblImagem.setBounds(10, 115, 46, 14);
+		lblImagem.setBounds(10, 169, 46, 14);
 		panel.add(lblImagem);
 		
 		txtImagem = new JTextField();
 		txtImagem.setColumns(10);
-		txtImagem.setBounds(10, 140, 311, 20);
+		txtImagem.setBounds(10, 187, 311, 20);
 		panel.add(txtImagem);
 		
 		JButton btnProcurar = new JButton("Procurar");
-		btnProcurar.setBounds(327, 139, 89, 23);
-		panel.add(btnProcurar);
-		
-		JButton btnNovo = new JButton("Novo");
-		btnNovo.addActionListener(new ActionListener() {
+		btnProcurar.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
+				//BUSCANDO IMAGEM NO COMPUTADOR
 				
-				modo = "novo";
-				manipulaView();
+				try {
+				JFileChooser caminhoImagem = new JFileChooser();
+				FileNameExtensionFilter filtro = new FileNameExtensionFilter("JPG & GIF Images", "jpg", "gif","png");//definindo um filtro de extensão
+				caminhoImagem.setFileFilter(filtro);
+				caminhoImagem.showOpenDialog(new Panel());
+				txtImagem.setText(caminhoImagem.getSelectedFile().getAbsolutePath());//pega o path ou a string do caminho
+				}catch(Exception ex) {
+					JOptionPane.showMessageDialog(null, "Não foi possivel Carregar a Imagem");
+					
+				}
 			}
 		});
-		btnNovo.setBounds(572, 139, 89, 23);
-		panel.add(btnNovo);
+		btnProcurar.setBounds(341, 186, 89, 23);
+		panel.add(btnProcurar);
 		
 		JPanel panel_1 = new JPanel();
 		panel_1.setBorder(new BevelBorder(BevelBorder.RAISED, null, null, null, null));
-		panel_1.setBounds(557, 126, 222, 91);
+		panel_1.setBounds(557, 151, 222, 152);
 		panel.add(panel_1);
+		panel_1.setLayout(null);
 		
-		JScrollPane scrollPane = new JScrollPane();
-		scrollPane.setBounds(10, 275, 789, 245);
-		contentPane.add(scrollPane);
+		JButton btnSalvar_1 = new JButton("Salvar");
+		btnSalvar_1.setBounds(10, 11, 94, 23);
+		panel_1.add(btnSalvar_1);
 		
-		tableProduto = new JTable();
-		tableProduto.setFont(new Font("Tahoma", Font.PLAIN, 11));
-		tableProduto.setModel(new DefaultTableModel(
-			new Object[][] {
-			},
-			new String[] {
-				"Produto", "Categoria", "Marca", "Pre\u00E7o", "Saldo Estoque"
-			}
-		) {
-			Class[] columnTypes = new Class[] {
-				String.class, String.class, String.class, Float.class, Object.class
-			};
-			public Class getColumnClass(int columnIndex) {
-				return columnTypes[columnIndex];
+		JButton btnVisualizar = new JButton("Visualizar");
+		btnVisualizar.setBounds(118, 11, 94, 23);
+		panel_1.add(btnVisualizar);
+		
+		JButton btnExcluir = new JButton("Excluir");
+		btnExcluir.setBounds(10, 57, 89, 23);
+		panel_1.add(btnExcluir);
+		
+		JButton btnCancelar = new JButton("Cancelar");
+		btnCancelar.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				dispose();
 			}
 		});
-		scrollPane.setViewportView(tableProduto);
+		btnCancelar.setBounds(118, 57, 94, 23);
+		panel_1.add(btnCancelar);
+		
+		JLabel lblUm = new JLabel("U.M");
+		lblUm.setBounds(493, 25, 35, 14);
+		panel.add(lblUm);
+		
+		txtUnidMedida = new JTextField();
+		txtUnidMedida.setColumns(10);
+		txtUnidMedida.setBounds(493, 39, 46, 20);
+		panel.add(txtUnidMedida);
+		btnVisualizar.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				
+				GerProduto gerProduto = new GerProduto();
+				gerProduto.setVisible(true);
+				gerProduto.setLocationRelativeTo(null);
+				gerProduto.atualizarTabelaProduto();
+			}
+		});
+		btnSalvar_1.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				
+				Produto produto = new Produto();
+				Categoria categ = new Categoria();
+				Marca marca = new Marca();
+		
+				
+				
+		        ProdutoController produtoController = new ProdutoController();
+		        //pegando o que foi inserido
+		        
+		        produto.setNomeProduto(txtNomeProd.getText());
+		        produto.setDescricaoProduto(txtDescProd.getText());
+		        produto.setImagem(txtImagem.getText());
+		        produto.setPrecoVenda(Float.parseFloat(txtValVenda.getText()));
+		        produto.setEstoque(Integer.parseInt (txtSaldoEstoque.getText()));
+		        produto.setUnidadeMedida(txtUnidMedida.getText());
+		        //LEITURA COMBO BOX
+		        categ = (Categoria) cbCategoria.getSelectedItem();
+		        produto.setCategoria(categ);
+		        
+		        marca = (Marca) cbMarca.getSelectedItem();
+		        produto.setMarca(marca);
+		        	
+		        produtoController.inserirProdutoController(produto);  
+		      
+		        txtNomeProd.setText("");
+		        txtDescProd.setText("");
+		        cbCategoria.addItem("");
+		        cbMarca.addItem("");
+		        txtValVenda.setText("");
+		        txtSaldoEstoque.setText("");
+		        txtUnidMedida.setText("");
+		        
+				
+			}
+		});
 		
 		dispose();
-	}
-	
-	public void limpaCampo() {
-		txtNomeProd.setText("");
-		txtDescProd.setText("");
-		txtValVenda.setText("");
-		txtSaldoEstoque.setText("");
-		txtImagem.setText("");
-	}
-	
-	public void manipulaView() {
-		switch(modo) {
-		case "novo":
-			txtNomeProd.setEnabled(true);
-			txtDescProd.setEnabled(true);
-			txtValVenda.setEnabled(true);
-			txtSaldoEstoque.setEnabled(true);
-			txtImagem.setEnabled(true);
-			btnSalvar.setEnabled(true);
-			btnNovo.setEnabled(false);
-			
-			
-			break;
-			
-		case "salvar":
-			txtNomeProd.setEnabled(false);
-			txtDescProd.setEnabled(false);
-			txtValVenda.setEnabled(false);
-			txtSaldoEstoque.setEnabled(false);
-			txtImagem.setEnabled(false);
-			btnSalvar.setEnabled(false);
-			btnNovo.setEnabled(true);
-			
-			
-		
-			
-		}
+		this.setLocationRelativeTo(null);
 	}
 }
