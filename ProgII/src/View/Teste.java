ae9d43180ff09065f8eb1140741ac489184a04e8
Caio Lima <caioifg@gmail.com>
2019-07-06 16:56:42 -0300

    a

--- ProgII/src/View/Teste.java
+++
@@ -1,9 +0,0 @@
-package View;
-
-/*public class Teste {
-	public static void main(String args[]) {
-		//ConnectionDB con = new ConnectionDB();
-		System.out.println(new UsuarioDAO().verificarLogin("wintonigor14@gmail.com", "123"));
-	}
-*/
-
