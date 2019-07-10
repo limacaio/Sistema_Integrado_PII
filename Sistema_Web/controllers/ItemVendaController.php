5dffa35ee57c06b0979559fffdcc006d0d814f2e
elizeuO <elizeu_os@hotmail.com>
2019-07-10 19:52:32 -0300

    Update ItemVendaController.php

--- Sistema_Web/controllers/ItemVendaController.php
+++ Sistema_Web/controllers/ItemVendaController.php
@@ -3,6 +3,20 @@
 
 class ItemVendaController
 {
+    public static function inserir(ItemVenda $itemVenda){
+        $sql = "INSERT INTO itemvenda (idItemVenda, idProduto, idVendas, quantidade,valor) 
+                VALUES (:idItemVenda, :idProduto, :idVenda, :quantidade,:valor)";
 
+        $db = Conexao::getInstance();
+        $stmt = $db->prepare($sql);
+        $stmt->bindValue(':idItemVenda', $itemVenda->getVenda()->getId());
+        $stmt->bindValue(':idProduto', $itemVenda->getProduto()->getIdProduto());
+        $stmt->bindValue(':quantidade', $itemVenda->getQuantidade());
+        $stmt->bindValue(':valor', $itemVenda->getValorProduto());
+
+        $stmt->execute();
+
+        return "OK";
+    }
 
 }
