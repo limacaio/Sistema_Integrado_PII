2b9532db750737663e00153c290457f93782af05
elizeuO <elizeu_os@hotmail.com>
2019-07-10 18:40:56 -0300

    ...

--- Sistema_Web/views/header.php
+++ Sistema_Web/views/header.php
@@ -53,49 +53,11 @@ if (isset($_SESSION['user'])){
                 </div>
             </nav>
             <div id="cart" class="btn-group btn-block">
+                <a href="cart.php"
                 <button type="button" class="btn btn-inverse btn-block btn-lg dropdown-toggle cart-dropdown-button">
-                    <span id="cart-total"><span>Carrinho de Compras</span><br>0 item(s) - R$0,00</span></button>
-                <ul class="dropdown-menu pull-right cart-dropdown-menu">
-                    <li>
-                        <table class="table table-striped">
-                            <tbody>
-                            <tr>
-                                <td class="text-center"><a href="#"><img class="img-thumbnail" title="lorem ippsum dolor dummy" alt="lorem ippsum dolor dummy" src="image/product/7product56x72.jpg"></a></td>
-                                <td class="text-left"><a href="#">lorem ippsum dolor dummy</a></td>
-                                <td class="text-right">x 1</td>
-                                <td class="text-right">$254.00</td>
-                                <td class="text-center"><button class="btn btn-danger btn-xs" title="Remove" type="button"><i class="fa fa-times"></i></button></td>
-                            </tr>
-                            </tbody>
-                        </table>
-                    </li>
+                    <span id="cart-total"><span>Carrinho de Compras</span><br>Visualizar</span></button>
+                </a>
 
-                    <li>
-                        <div>
-                            <table class="table table-bordered">
-                                <tbody>
-                                <tr>
-                                    <td class="text-right"><strong>Sub-Total</strong></td>
-                                    <td class="text-right">$210.00</td>
-                                </tr>
-                                <tr>
-                                    <td class="text-right"><strong>Eco Tax (-2.00)</strong></td>
-                                    <td class="text-right">$2.00</td>
-                                </tr>
-                                <tr>
-                                    <td class="text-right"><strong>VAT (20%)</strong></td>
-                                    <td class="text-right">$42.00</td>
-                                </tr>
-                                <tr>
-                                    <td class="text-right"><strong>Total</strong></td>
-                                    <td class="text-right">$254.00</td>
-                                </tr>
-                                </tbody>
-                            </table>
-                            <p class="text-right"> <span class="btn-viewcart"><a href="cart.php"><strong><i class="fa fa-shopping-cart"></i> Meu Carrinho</strong></a></span> <span class="btn-checkout"><a href="checkout.php"><strong><i class="fa fa-share"></i> Finalizar</strong></a></span> </p>
-                        </div>
-                    </li>
-                </ul>
             </div>
         </div>
     </div>
